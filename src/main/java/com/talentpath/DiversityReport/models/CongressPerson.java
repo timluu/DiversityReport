354596452573d88f2127ecce960c0085eeae03a7
Tim <timbo.luu@gmail.com>
2020-12-14 15:05:22 -0500

    add data models

---
+++ src/main/java/com/talentpath/DiversityReport/models/CongressPerson.java
@@ -0,0 +1,105 @@
+package com.talentpath.DiversityReport.models;
+
+public class CongressPerson {
+    String fullName;
+    String state;
+    String governmentSeat;
+    Integer yearBegan;
+    Integer yearEnded;
+    String ethnicity;
+    String gender;
+    Integer birthYear;
+    Integer appointmentAge;
+    Integer totalTerms;
+    String photoUrl;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getGovernmentSeat() {
+        return governmentSeat;
+    }
+
+    public void setGovernmentSeat(String governmentSeat) {
+        this.governmentSeat = governmentSeat;
+    }
+
+    public Integer getYearBegan() {
+        return yearBegan;
+    }
+
+    public void setYearBegan(Integer yearBegan) {
+        this.yearBegan = yearBegan;
+    }
+
+    public Integer getYearEnded() {
+        return yearEnded;
+    }
+
+    public void setYearEnded(Integer yearEnded) {
+        this.yearEnded = yearEnded;
+    }
+
+    public String getEthnicity() {
+        return ethnicity;
+    }
+
+    public void setEthnicity(String ethnicity) {
+        this.ethnicity = ethnicity;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public Integer getAppointmentAge() {
+        return appointmentAge;
+    }
+
+    public void setAppointmentAge(Integer appointmentAge) {
+        this.appointmentAge = appointmentAge;
+    }
+
+    public Integer getTotalTerms() {
+        return totalTerms;
+    }
+
+    public void setTotalTerms(Integer totalTerms) {
+        this.totalTerms = totalTerms;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public CongressPerson() {}
+}
---
+++ src/main/java/com/talentpath/DiversityReport/models/Governor.java
@@ -0,0 +1,123 @@
+package com.talentpath.DiversityReport.models;
+
+public class Governor {
+    String fullName;
+    String state;
+    Integer yearBegan;
+    Integer yearEnded;
+    String ethnicity;
+    String gender;
+    Integer birthYear;
+    Integer appointmentAge;
+    Integer isAlive;
+    Integer income;
+    String religion;
+    String previousOccupation;
+    String photoUrl;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getYearBegan() {
+        return yearBegan;
+    }
+
+    public void setYearBegan(Integer yearBegan) {
+        this.yearBegan = yearBegan;
+    }
+
+    public Integer getYearEnded() {
+        return yearEnded;
+    }
+
+    public void setYearEnded(Integer yearEnded) {
+        this.yearEnded = yearEnded;
+    }
+
+    public String getEthnicity() {
+        return ethnicity;
+    }
+
+    public void setEthnicity(String ethnicity) {
+        this.ethnicity = ethnicity;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public Integer getAppointmentAge() {
+        return appointmentAge;
+    }
+
+    public void setAppointmentAge(Integer appointmentAge) {
+        this.appointmentAge = appointmentAge;
+    }
+
+    public Integer getIsAlive() {
+        return isAlive;
+    }
+
+    public void setIsAlive(Integer isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    public Integer getIncome() {
+        return income;
+    }
+
+    public void setIncome(Integer income) {
+        this.income = income;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public String getPreviousOccupation() {
+        return previousOccupation;
+    }
+
+    public void setPreviousOccupation(String previousOccupation) {
+        this.previousOccupation = previousOccupation;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public Governor() {}
+}
