24ba7d07fdb571452007d5581bb8322c91097b92
Fei Yang <fyang@C02CMKHKML7H.local>
2020-12-15 13:58:38 -0800

    testing

--- src/main/java/com/talentpath/DiversityReport/controllers/WebController.java
+++ src/main/java/com/talentpath/DiversityReport/controllers/WebController.java
@@ -2,9 +2,17 @@ package com.talentpath.DiversityReport.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World Index!";
+    }
+
     @GetMapping("")
     public String homePage() {
         return "index";
