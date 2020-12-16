16479921b31010a45a64b98fda4e9bc3cd0dcd6b
Tim <timbo.luu@gmail.com>
2020-12-16 11:53:55 -0500

    test div change

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -16,48 +16,48 @@ const displayTable = (position, members) => {
     
     const table = $(`#${position}Table`);
     table.append(`<caption>${position.toUpperCase()}</caption>`);
-    const header = $("<tr></tr>");
+    const header = $('<div class="header"></div>');
     
-    header.append("<th>ID</th>");
-    header.append("<th>Full Name</th>");
-    if (position === "mayors") header.append("<th>City</th>");
-    header.append("<th>State</th>");
-    header.append("<th>Year Began</th>");
-    header.append("<th>Year Ended</th>");
-    header.append("<th>Ethnicity</th>");
-    header.append("<th>Gender</th>");
-    header.append("<th>Birth Year</th>");
-    header.append("<th>Appointment Age</th>");
-    header.append("<th>Religion</th>");
+    header.append("<div>ID</div>");
+    header.append("<div>Full Name</div>");
+    if (position === "mayors") header.append("<div>City</div>");
+    header.append("<div>State</div>");
+    header.append("<div>Year Began</div>");
+    header.append("<div>Year Ended</div>");
+    header.append("<div>Ethnicity</div>");
+    header.append("<div>Gender</div>");
+    header.append("<div>Birth Year</div>");
+    header.append("<div>Appointment Age</div>");
+    header.append("<div>Religion</div>");
     if (position === "mayors" || position === "governors") {
-        header.append("<th>Alive</th>");
-        header.append("<th>Income</th>");
-        header.append("<th>Previous Occupation</th>");
+        header.append("<div>Alive</div>");
+        header.append("<div>Income</div>");
+        header.append("<div>Previous Occupation</div>");
     }
-    if (position === "senators" || position === "representatives") header.append("<th>Total Terms</th>");
-    header.append("<th>Photo Url</th>");
+    if (position === "senators" || position === "representatives") header.append("<div>Total Terms</div>");
+    header.append("<div>Photo Url</div>");
     table.append(header);
 
     for (let member of members) {
-        let row = $("<tr></tr>");
-        row.append(`<td>${member.id}</td>`);
-        row.append(`<td>${member.fullName}</td>`);
-        if (position === "mayors") row.append(`<td>${member.city}</td>`);
-        row.append(`<td>${member.state}</td>`);
-        row.append(`<td>${member.yearBegan}</td>`);
-        row.append(`<td>${member.yearEnded}</td>`);
-        row.append(`<td>${member.ethnicity}</td>`);
-        row.append(`<td>${member.gender}</td>`);
-        row.append(`<td>${member.birthYear}</td>`);
-        row.append(`<td>${member.appointmentAge}</td>`);
-        row.append(`<td>${member.religion}</td>`);
+        let row = $('<div class="row"></tr>');
+        row.append(`<div>${member.id}</div>`);
+        row.append(`<div>${member.fullName}</div>`);
+        if (position === "mayors") row.append(`<div>${member.city}</div>`);
+        row.append(`<div>${member.state}</div>`);
+        row.append(`<div>${member.yearBegan}</div>`);
+        row.append(`<div>${member.yearEnded}</div>`);
+        row.append(`<div>${member.ethnicity}</div>`);
+        row.append(`<div>${member.gender}</div>`);
+        row.append(`<div>${member.birthYear}</div>`);
+        row.append(`<div>${member.appointmentAge}</div>`);
+        row.append(`<div>${member.religion}</div>`);
         if (position === "mayors" || position === "governors") {
-            row.append(`<td>${member.isAlive}</td>`);
-            row.append(`<td>${member.income}</td>`);
-            row.append(`<td>${member.previousOccupation}</td>`);
+            row.append(`<div>${member.isAlive}</div>`);
+            row.append(`<div>${member.income}</div>`);
+            row.append(`<div>${member.previousOccupation}</div>`);
         }
-        if (position === "senators" || position === "representatives") row.append(`<td>${member.totalTerms}</td>`);
-        row.append(`<td>${member.photoUrl}</td>`);
+        if (position === "senators" || position === "representatives") row.append(`<div>${member.totalTerms}</div>`);
+        row.append(`<div>${member.photoUrl}</div>`);
         table.append(row);
     }
 
